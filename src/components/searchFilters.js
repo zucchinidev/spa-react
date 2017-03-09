8f565ccc9cd79e138c417f52ed1b0d5837c65814
Andrea Zucchini <azga84@gmail.com>
2017-03-09 23:58:47 +0100

    dispatch  filter action

--- src/components/searchFilters.js
+++ src/components/searchFilters.js
@@ -3,10 +3,14 @@ class SearchFilters extends Component {
 
   constructor (...args) {
     super(...args)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleSubmit (event) {
-
+    event.preventDefault()
+    this.props.onFilter({
+      text: this.input.value
+    })
   }
 
   render () {
@@ -16,7 +20,7 @@ class SearchFilters extends Component {
           <div className="col s12">
             <form onSubmit={this.handleSubmit}>
               <div className="input-field">
-                <input ref={(node) => (this.input = node)} id="search" type="search" required />
+                <input ref={(node) => (this.input = node)} id="search" type="search" />
                 <label htmlFor="search"><i className="material-icons">search</i></label>
                 <i className="material-icons">close</i>
               </div>
