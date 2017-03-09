e837314edfbe440ebb753ac38aee42846a87cd46
Andrea Zucchini <azga84@gmail.com>
2017-03-09 23:58:58 +0100

    fix typo

--- src/containers/searcher.js
+++ src/containers/searcher.js
@@ -1,12 +1,14 @@
 import React, { Component, PropTypes } from 'react'
 import { SearchFilters, CardList } from '../components'
 import { connect } from 'react-redux'
+import { filterDogs } from '../actions/searcher'
+
 
 class Searcher extends Component {
   render () {
     return (
       <div>
-        <SearchFilters />
+        <SearchFilters onFilter={this.props.onFilter} />
         <div className="container">
           <CardList dogs={this.props.dogs} />
         </div>
@@ -21,10 +23,16 @@ Searcher.propTypes = {
     avatar: PropTypes.string,
     description: PropTypes.string,
     id: PropTypes.string
-  }))
+  })),
+  onFilter: PropTypes.func
 }
 
 const mapStateToProps = (state) => state
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onFilter: (filter) => dispatch(filterDogs(filter))
+  }
+}
 
-export default connect(mapStateToProps)(Searcher)
+export default connect(mapStateToProps, mapDispatchToProps)(Searcher)
 
--- src/reducers/searcher.js
+++ src/reducers/searcher.js
@@ -1,9 +1,18 @@
 import dogs from '../data'
+import { FILTER_DOG } from '../actions/actionTypes'
 const searcher = (state = { dogs }, action) => {
   switch (action.type) {
+    case FILTER_DOG:
+      return {
+        dogs: state.dogs.filter(dog => applyFilter(dog.name, action.payload.text))
+      }
     default:
       return state
   }
+}
+
+const applyFilter = (name, filter) => {
+  return filter !== '' ? name.toLowerCase().includes(filter.toLowerCase()) : true
 }
 
 export default searcher
