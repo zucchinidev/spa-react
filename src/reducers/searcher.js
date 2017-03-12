cfa9db3e602247d51da94eddcc71a359ba708222
Andrea Zucchini <azga84@gmail.com>
2017-03-12 16:32:40 +0100

    preserve original data

--- src/reducers/searcher.js
+++ src/reducers/searcher.js
@@ -1,10 +1,15 @@
 import dogs from '../data'
 import { FILTER_DOG } from '../actions/actionTypes'
-const searcher = (state = { dogs }, action) => {
+
+const searcher = (state = { dogs, originalDogs: dogs }, action) => {
   switch (action.type) {
     case FILTER_DOG:
+      const filter = action.payload.text
+      const dogs = state.originalDogs.filter(dog => applyFilter(dog.name, filter))
       return {
-        dogs: state.dogs.filter(dog => applyFilter(dog.name, action.payload.text))
+        filter,
+        dogs,
+        originalDogs: [...state.originalDogs]
       }
     default:
       return state
@@ -12,7 +17,7 @@ const searcher = (state = { dogs }, action) => {
 }
 
 const applyFilter = (name, filter) => {
-  return filter !== '' ? name.toLowerCase().includes(filter.toLowerCase()) : true
+  return name.toLowerCase().includes(filter.toLowerCase())
 }
 
 export default searcher
