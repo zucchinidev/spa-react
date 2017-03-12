b0a4abd698bfee00ff98789ac756155ae045dbea
Andrea Zucchini <azga84@gmail.com>
2017-03-12 16:24:45 +0100

    exports new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,11 @@
 import SearchFilters from './searchFilters'
 import CardList from './cardList'
+import DogTitle from './dogTitle'
+import DogDescription from './dogDescription'
 
 export {
   SearchFilters,
-  CardList
+  CardList,
+  DogTitle,
+  DogDescription
 }
