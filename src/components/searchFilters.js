ddece1f0ef359b459d5713ccc5b06b0daeed7479
Andrea Zucchini <azga84@gmail.com>
2017-03-12 16:25:36 +0100

    add onChange event

--- src/components/searchFilters.js
+++ src/components/searchFilters.js
@@ -4,10 +4,17 @@ class SearchFilters extends Component {
   constructor (...args) {
     super(...args)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
   handleSubmit (event) {
     event.preventDefault()
+    this.props.onFilter({
+      text: this.input.value
+    })
+  }
+
+  handleChange () {
     this.props.onFilter({
       text: this.input.value
     })
@@ -20,7 +27,7 @@ class SearchFilters extends Component {
           <div className="col s12">
             <form onSubmit={this.handleSubmit}>
               <div className="input-field">
-                <input ref={(node) => (this.input = node)} id="search" type="search" />
+                <input onChange={this.handleChange} ref={(node) => (this.input = node)} id="search" type="search" />
                 <label htmlFor="search"><i className="material-icons">search</i></label>
                 <i className="material-icons">close</i>
               </div>
